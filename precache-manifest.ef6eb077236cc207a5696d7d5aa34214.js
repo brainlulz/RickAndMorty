35bf8526ac5d0b51d56a3d32dfdad384964a3d16
Alex Labonne <>
2020-04-21 21:43:42 +0200

    Updates

---
+++ precache-manifest.ef6eb077236cc207a5696d7d5aa34214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f491c8138d6aa82c6486c3c076d02eed",
+    "url": "/rick-and-morty-react/index.html"
+  },
+  {
+    "revision": "21dc24075cc2b9688097",
+    "url": "/rick-and-morty-react/static/css/main.0f7019e7.chunk.css"
+  },
+  {
+    "revision": "61f75d83e3c3cfd8f48e",
+    "url": "/rick-and-morty-react/static/js/2.eb621d12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty-react/static/js/2.eb621d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21dc24075cc2b9688097",
+    "url": "/rick-and-morty-react/static/js/main.0af8fafb.chunk.js"
+  },
+  {
+    "revision": "610201537f2d50256337",
+    "url": "/rick-and-morty-react/static/js/runtime-main.fbd78f2a.js"
+  }
+]);
