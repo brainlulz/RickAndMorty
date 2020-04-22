b6fcc623a51f29ae386076f69b6e6520f99b956b
Alex Labonne <>
2020-04-22 22:19:45 +0200

    chore: remove useless homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import styles from './App.module.css';
 
-
 import Characters from './components/Characters';
-import Home from './components/Home';
 
 function App() {
   return (
-    <Router basename='/' >
+    <Router basename="/">
       <div>
-        <nav>
-          <ul className={styles.navbar}>
-            <li className={styles.navbar}>
-              <Link to="/">Home</Link>
-            </li>
-            <li className={styles.navbar}>
-              <Link to="/characters">Characters</Link>
-            </li>
-          </ul>
-        </nav>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/characters">
-            <Characters />
-          </Route>
           <Route path="/">
-            <Home />
+            <Characters />
           </Route>
         </Switch>
       </div>
