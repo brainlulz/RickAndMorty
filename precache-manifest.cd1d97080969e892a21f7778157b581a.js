5f360d130dbd947b49ea98d7ad1074151696306b
Alex Labonne <>
2020-04-23 23:52:47 +0200

    Updates

--- precache-manifest.cd1d97080969e892a21f7778157b581a.js
+++ precache-manifest.cd1d97080969e892a21f7778157b581a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b309b8236cf989511b9e99d47260053a",
+    "revision": "4bc9b3ea017d5283a4896a501de0eab3",
     "url": "/RickAndMorty/index.html"
   },
   {
-    "revision": "aa0c5caafd8273f751d3",
+    "revision": "dd98f00ba1cbbb76b6f9",
     "url": "/RickAndMorty/static/css/main.05701c79.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/RickAndMorty/static/js/2.326368f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aa0c5caafd8273f751d3",
-    "url": "/RickAndMorty/static/js/main.9723cfc3.chunk.js"
+    "revision": "dd98f00ba1cbbb76b6f9",
+    "url": "/RickAndMorty/static/js/main.a74a22ec.chunk.js"
   },
   {
     "revision": "92890f482eaac098fdfe",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RickAndMorty/precache-manifest.d398bd4b25f160bf0bc08f0802e7ed1f.js"
+  "/RickAndMorty/precache-manifest.cd1d97080969e892a21f7778157b581a.js"
 );
 
 self.addEventListener('message', (event) => {
